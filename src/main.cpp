1999edfa28eef8adabaad667511b78770dee50c5
sidatii <sidatnouhi@gmail.com>
2026-01-04 00:21:12 +0100

    stored the return of connect to a variable to avoid unexpected behavior , cleaned the code and added comments

--- src/main.cpp
+++ src/main.cpp
@@ -9,17 +9,26 @@
 #include "../include/Printer.h"
 #include "../include/Scanner.h"
 
+#define START_PORT 1
+#define END_PORT 65535
+#define RED "\033[31m"
+#define BLUE "\033[36m"
+#define PINK "\033[33m"
+#define NC "\033[0m"
+
 using namespace std;
 
 #define THREAD_NUM 200
 
 void print_usage() {
-  std::cout << "Usage: xpscan <IP> [options]\n"
-            << "Options:\n"
+  std::cout << PINK << "Usage: xpscan <IP> [options]\n"
+            << NC
+            << BLUE << "Options:\n"
             << "  --help    Print this message\n"
             << "  --json    Export results to [IP].json\n"
             << "  --txt     Export results to [IP].txt\n"
-            << "  --full    Scan all 65535 ports\n";
+            << "  --full    Scan all 65535 ports\n"
+            << NC;
 }
 
 int main(int argc, char* argv[]) {
@@ -28,14 +37,21 @@ int main(int argc, char* argv[]) {
     print_usage();
     return 1;
   }
+
   // Validate the ip
   std::regex ip_regex(R"(^(\d{1,3}\.){3}\d{1,3}$)");
   if (!std::regex_match(argv[1], ip_regex)) {
-    std::cerr << "\033[31m[x] Invalid IP address format.\033[0m\n";
+    std::cerr << RED << "[x] Invalid IP address format." << NC << endl;
     return 1;
   }
+
+  // Instatiate user input struct
   struct UserInput input{};
+
+  // Parse args
   std::vector<std::string> args(argv + 2, argv + argc);
+
+  // Set IP attribute to the second arg passed
   input.ip = argv[1];
 
   // Parse Flags
@@ -45,34 +61,40 @@ int main(int argc, char* argv[]) {
 
   Printer::printHeader();
 
+  // Check for fullscan flag
   if (fullScan) {
-    input.start_port = 1;
-    input.end_port = 65535;
+    input.start_port = START_PORT;
+    input.end_port = END_PORT;
   } else {
-    std::cout << "\033[36mEnter start port:\033[0m ";
+    std::cout << BLUE << "Enter start port:" << NC;
     std::cin >> input.start_port;
-    std::cout << "\033[36mEnter end port:\033[0m ";
+    std::cout << BLUE << "Enter end port:" << NC;
     std::cin >> input.end_port;
   }
-  // cout << "\nEnter the path to save the result (default: " << SAVE_PATH << ")" << ", click enter to use default.\nPath: ";
-  // cin >> input.path_choice;
-  // if (input.path_choice == "\n") {
-  // }
 
+  // Instatiate the scanner class with params
   Scanner scanner(input.ip, THREAD_NUM);
-  // Start time recording
+
+  // Record start time
   auto start = std::chrono::steady_clock::now();
+
+  // Proceed with the scanning logic
   scanner.scanRange(input.start_port, input.end_port);
+
+  // Record end time
   auto end = std::chrono::steady_clock::now();
 
+  // Calculate elapsed time
   std::chrono::duration<double> diff = end - start;
-  std::cout << "\n\033[36m[!] Scan finished in " << diff.count() << "s\033[0m" << std::endl;
+  std::cout << "\n"
+            << BLUE << "[!] Scan finished in " << diff.count() << "s" << NC << std::endl;
 
+  // Check for export options and proceed with it
   if (exportJson) Exporter::saveToJson(input.ip, scanner.getResults());
   if (exportTxt) Exporter::saveToText(input.ip, scanner.getResults());
 
   if (!exportJson && !exportTxt) {
-    std::cout << "\033[33m[!] No export flags set. Use --json or --txt to save results.\033[0m" << std::endl;
+    std::cout << PINK << "[!] No export flags set. Use --json or --txt to save results." << NC << std::endl;
   }
 
   return 0;
